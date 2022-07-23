dccfaf7a45881333e3b17c8d44ff0e20b07a5de7
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-07-23 04:12:06 -0300

    layout perguntas done

--- src/Componentes/Question/Question.js
+++ src/Componentes/Question/Question.js
@@ -1,6 +1,9 @@
+import "./styles.css";
 export default function Question(props){
     return(
-        <>
-        </>
+        <div className="question">
+            <p>Pergunta</p>
+            <ion-icon name="play-outline"></ion-icon>
+        </div>
     )
 }
--- src/Componentes/QuestionsDisplay/QuestionsDisplay.js
+++ src/Componentes/QuestionsDisplay/QuestionsDisplay.js
@@ -5,10 +5,16 @@ import Question from "../Question/Question";
 
 export default function QuestionsDisplay(){
     const questionData = [
-        {},
-        {},
+        {question:"O que é JSX?", answer:"Uma extensão de linguagem do JavaScript"},
+        {question:"O React é ...", answer:"Uma biblioteca JavaScript para construção de interfaces"},
+        {question:"Componentes devem iniciar com ...", answer:"Letra maiúscula"},
+        {question:"Podemos colocar ... dentro do JSX", answer:"Expressões"},
+        {question:"O ReactDOM nos ajuda ...", answer:"Interagindo com a DOM para colocar componentes React na mesma"},
+        {question:"Usamos o npm para ...", answer:"Gerenciar os pacotes necessários e suas dependências"},
+        {question:"Usamos props para ...", answer:"Passar diferentes informações para componentes "},
+        {question:"Usamos estado (state) para ...", answer:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"},
     ];
-    let questionProps = questionData.map(question => <Question userName={post.userName} />);
+    let questionProps = questionData.map(question => <Question question={question.question} answer={question.answer} />);
     return(
         <div className="questions-display">
             <Link to="/">Voltar</Link>
